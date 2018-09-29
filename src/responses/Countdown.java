03dcf69e5eb07980397a09fd1771c9ff0e8eb50f
Albert F. Cervantes <albert.cervantes@gmail.com>
2018-09-29 14:34:07 -0700

    Response and Upload examples

--- src/responses/Countdown.java
+++ src/responses/Countdown.java
@@ -1,4 +1,4 @@
-package requests;
+package responses;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,12 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/SimpleAdder")
-public class SimpleAdder extends HttpServlet {
+@WebServlet("/responses/Countdown")
+public class Countdown extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
+
+	int count = 5;
+	
+	int delay = 1;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		// Set the content type to HTML
 		response.setContentType("text/html");
 		
@@ -27,40 +31,33 @@ public class SimpleAdder extends HttpServlet {
 		out.println("<head>");
 		out.println("    <meta charset=\"UTF-8\">");
 		out.println("	 <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">");
-		out.println("    <title>Simple Adder</title>");
+		out.println("    <title>Countdown</title>");
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<div class=\"container\">");
 		
 		// Page-Specific Content Goes Here...
-		out.println("<h1>Simple Adder</h1>");
+		out.println("<h1>Countdown</h1>");
 		
-		String num1 = request.getParameter("num1");
-		String num2 = request.getParameter("num2");
-		
-		try {
-			int sum = Integer.parseInt( num1 ) + Integer.parseInt( num2 );
-		
-			out.println(num1 + " + " + num2 + " = " + sum);
+		if (count > 0) {
+			out.println("<p class=\"lead\">T-minus " + count + " second(s) and counting...</p>");
+			--count;
+			response.setIntHeader( "Refresh", delay );
 		}
-		catch(Exception e) {
-			response.sendRedirect("SimpleAdder.html");
-			return;
-			//out.println("<p class=\"text-danger\">Please enter integral values</p>");
+		else {
+			out.println("<p class=\"lead text-danger\">Blast Off</p>");
+			count = 5;
 		}
 		
 		out.println("</div>");
 		out.println("</body>");
 		out.println("</html>");
+	
 	}
+
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
 }
-
-
-
-
-
-
